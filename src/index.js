5d806aad76033f6f69f7b48a5aed44ed458e1d32
Den <52295780+josserden@users.noreply.github.com>
2022-02-17 21:30:02 +0200

    finish lesson 8-2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './sass/main.scss';
+
+const form = document.querySelector('.form');
+
+initForm();
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    formData.forEach((key, value) => console.log(`${value}-${key}`));
+});
+
+form.addEventListener('reset', () => {
+    localStorage.removeItem('selectedFilters');
+});
+
+form.addEventListener('change', event => {
+    let parsedFilters = localStorage.getItem('selectedFilters');
+
+    parsedFilters = parsedFilters ? JSON.parse(parsedFilters) : {};
+
+    parsedFilters[event.target.name] = event.target.value;
+
+    if (!parsedFilters) return;
+
+    localStorage.setItem('selectedFilters', JSON.stringify(parsedFilters));
+});
+
+function initForm() {
+    let parsedFilters = localStorage.getItem('selectedFilters');
+
+    if (parsedFilters) {
+        parsedFilters = JSON.parse(parsedFilters);
+    }
+
+    Object.entries(parsedFilters).forEach(
+        ([name, value]) => (form.elements[name].value = value),
+    );
+}
