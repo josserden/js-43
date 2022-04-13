e347ebb189670eb4f3a7ac18967cd19416967fb0
Den <52295780+josserden@users.noreply.github.com>
2022-04-13 21:38:18 +0300

    finish 11-1

--- src/index.js
+++ src/index.js
@@ -5,3 +5,30 @@ import { getRefs } from './js/getRefs';
 import { renderContacts } from './js/renderContacts';
 
 const { contactsContainer } = getRefs();
+
+// GET /api/contacts
+APIservice.getContacts().then(contacts => {
+  renderContacts(contactsContainer, contacts.data);
+});
+
+// GET /api/contacts/:id
+// APIservice.getContact(13);
+// APIservice.getContact(130);
+
+// POST /api/contacts
+// APIservice.createContact({
+//   name: 'Maria',
+//   email: 'maria@gmail.com',
+//   number: '066-999-9991',
+// });
+
+// DELETE /api/contacts/:id
+// APIservice.deleteContact(26);
+// APIservice.deleteContact(22);
+// APIservice.deleteContact(6).then(data => console.log(data));
+
+// PUT /api/contacts/:id
+// APIservice.updateContact(7, {
+//   name: 'Jimmy',
+//   number: '066-999-6666',
+// });
--- src/js/service/api-service.js
+++ src/js/service/api-service.js
@@ -1,14 +1,79 @@
 import axios from 'axios';
 
+// const BASE_URL = 'https://6251bfb67f7fa1b1ddde85d8.mockapi.io/api';
+
 axios.defaults.baseURL = 'https://6251bfb67f7fa1b1ddde85d8.mockapi.io/api';
-axios.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
+axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 
-// GET /api/v1/contacts
+// GET /api/contacts
+export const getContacts = () => {
+  return axios.get('/contacts');
+  //   return fetch(`${BASE_URL}/contacts`).then(response => {
+  //     if (!response.ok) {
+  //       throw Error(response.statusText);
+  //     }
 
-// GET /api/v1/contacts/:id
+  //     return response.json();
+  //   });
+};
 
-// POST /api/v1/contacts
+// GET /api/contacts/:id
+export const getContact = id => {
+  return axios.get(`/contacts/${id}`);
+  //   return fetch(`${BASE_URL}/contacts/${id}`).then(response => {
+  //     if (!response.ok) {
+  //       throw Error(response.statusText);
+  //     }
 
-// DELETE /api/v1/contacts/:id
+  //     return response.json();
+  //   });
+};
 
-// PUT /api/v1/contacts/:id
+// POST /api/contacts
+export const createContact = contact => {
+  return axios.post('/contacts', contact);
+  //   return fetch(`${BASE_URL}/contacts`, {
+  //     method: 'POST',
+  //     body: JSON.stringify(contact),
+  //     headers: {
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //   }).then(response => {
+  //     if (!response.ok) {
+  //       throw Error(response.statusText);
+  //     }
+  //     return response.json();
+  //   });
+};
+
+// DELETE /api/contacts/:id
+export const deleteContact = id => {
+  return axios.delete(`/contacts/${id}`);
+  //   return fetch(`${BASE_URL}/contacts/${id}`, {
+  //     method: 'DELETE',
+  //   }).then(response => {
+  //     if (!response.ok) {
+  //       throw Error(response.statusText);
+  //     }
+
+  //     return response.json();
+  //   });
+};
+
+// PUT /api/contacts/:id
+export const updateContact = (id, contact) => {
+  return axios.put(`/contacts/${id}`, contact);
+  //   return fetch(`${BASE_URL}/contacts/${id}`, {
+  //     method: 'PUT',
+  //     body: JSON.stringify(contact),
+  //     headers: {
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //   }).then(response => {
+  //     if (!response.ok) {
+  //       throw Error(response.statusText);
+  //     }
+
+  //     return response.json();
+  //   });
+};
